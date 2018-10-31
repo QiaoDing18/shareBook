a6a00a09b69c6bbe8bb2e2a9e4408416cabaf447
QiaoDing18 <tjoe18@126.com>
2018-10-31 13:12:27 +0800

    重构

--- server/app/router.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = app => {
-  require('./router/client')(app);
-};
--- server/app/router/client.js
+++
@@ -1,4 +0,0 @@
-module.exports = app => {
-  const { router, controller } = app;
-  const interceptor = app.middleware.interceptor();
-}
